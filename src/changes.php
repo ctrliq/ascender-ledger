9082eb2b796b40afa86009d30c04340cab3dc5a8
Jimmy <jconner@ciq.com>
2023-10-09 21:54:38 -0500

    Small fix for job data until clusters are implemented

--- src/changes.php
+++ src/changes.php
@@ -17,7 +17,8 @@ if (isset($_GET['action']) && $_GET['action'] == 'view' && $_GET['change'] == in
 	if (isset($change['id']) && $change['id']) {
 #		$change['res'] = unserialize(base64_decode($change['res']));
 		$server = db_fetch_assoc_prepare('SELECT * FROM `servers` WHERE `name` = ?', array($change['server']));
-		$job = db_fetch_assoc_prepare('SELECT * FROM `jobs` WHERE `job` = ? AND `host` = ?', array($change['job'], $change['server']));
+#		$job = db_fetch_assoc_prepare('SELECT * FROM `jobs` WHERE `job` = ? AND `host` = ?', array($change['job'], $change['server']));
+		$job = db_fetch_assoc_prepare('SELECT * FROM `jobs` WHERE `job` = ?', array($change['job']));
 		//$res = parse_res($change['task_action'], $change['res']);
 		$res = $change['res'];
 		echo $twig->render('change.html', array_merge($twigarr, array('change' => $change, 'server' => $server, 'hosts' => $h, 'res' => $res, 'job' => $job)));
